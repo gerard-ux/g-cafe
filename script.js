866efae9dadd6657ef40741b0c5d6bd5ff1d637f
gerard-ux <gerardrinos2@gmail.com>
2024-07-18 23:11:25 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// Swiper //
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
+
+// Menu Icon Open & Close //
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar'); 
+
+menu.onclick  = () => {
+  menu.classList.toggle('move')
+  navbar.classList.toggle("open-menu");
+};
+
+// Menu Icon Open & Close onscroll //
+window.onscroll = () => {
+  menu.classList.remove('move')
+  navbar.classList.remove("open-menu");
+};
+
+// ScrollReveal Animation //
+const animate = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: "2500",
+  delay: "400",
+});
+
+animate.reveal(".nav");
+animate.reveal(".home-text", { origin: "left"});
+animate.reveal(".home-img", { origin: "bottom"});
+animate.reveal(".home-img", { origin: "bottom"});
+animate.reveal(".service-box, .product-box, .team-box, .book-data", { interval: 100 });
